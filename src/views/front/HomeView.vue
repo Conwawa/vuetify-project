98d6422620cc4f5f20f22e7191efcaf8b73aedd6
Conwawa <cassiopeiahebe@gmail.com>
2023-08-17 16:23:00 +0800

    src

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -3,7 +3,7 @@
   <VRow>
     <VCol cols="12" style="padding: 0;">
       <v-carousel :show-arrows="false" hide-delimiters height="600">
-        <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover>
+        <v-carousel-item v-for="(item, i) in items" :key="i" :src="'public/seafood.png'" cover>
         </v-carousel-item>
       </v-carousel>
       <div class="header">
